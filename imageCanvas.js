96ff6fb93cd0f92774f1ac0b5900e0b513581100
bigpooh <myrhi123@naver.com>
2015-09-15 20:59:34 +0900

    Canvas첫시작

---
+++ imageCanvas.js
@@ -0,0 +1,10 @@
+﻿
+window.addEventListener("load",drawScreen,true);
+
+function drawScreen()
+{
+var theCanvas=document.getElementById("GameCanvas");
+var Context=theCanvas.getContext("2d");
+Context.fillStyle="#000";
+Context.fillRect(0,0,1000,700);
+}
